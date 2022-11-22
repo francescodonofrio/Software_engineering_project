26b06d81267ea20db65a21280e9b72c97609b5bf
Paolo <p.franco9@studenti.unisa.it>
2022-11-22 10:59:49 +0100

    Another strange package

--- Software_engineering_project/src/Software_engineering_project.java
+++ Software_engineering_project/src/Software_engineering_project.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package software_engineering_project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
