dce266bed6e1e1eb65853d5a099a30221ebb377c
francescodonofrio <francescodonofrio.200@gmail.com>
2022-11-22 10:19:43 +0100

    Initial GUI

--- Software_engineering_project/src/software_engineering_project/WindowController.java
+++ Software_engineering_project/src/software_engineering_project/WindowController.java
@@ -7,25 +7,18 @@ package software_engineering_project;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
 /**
  *
  * @author franc
  */
-public class FXMLDocumentController implements Initializable {
-    
+public class WindowController implements Initializable {
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World! da Paolo");
-    }
+    private Pane drawingPane;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
