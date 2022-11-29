b21b902aa93e6f0da63a031a97867e24b1834bb8
vinzcamp8 <vinzclash01@gmail.com>
2022-11-29 15:03:31 +0100

    Update constraint on size of GUI

--- Software_engineering_project/src/mainPackage/SoftwareEngineeringProject.java
+++ Software_engineering_project/src/mainPackage/SoftwareEngineeringProject.java
@@ -29,6 +29,8 @@ public class SoftwareEngineeringProject extends Application {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
+        stage.setMinHeight(500);
+        stage.setMinWidth(800);
         stage.show();
     }
 
