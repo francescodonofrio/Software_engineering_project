5d01627819e3e14d30c0662ecb272aa28ca4346f
martacorcione <m.corcione5@studenti.unisa.it>
2022-12-11 16:37:49 +0100

    update

--- Software_engineering_project/src/action/MoveAction.java
+++ Software_engineering_project/src/action/MoveAction.java
@@ -2,7 +2,6 @@ package action;
 
 import exceptions.NotExecutedActionException;
 import exceptions.NotMovedException;
-import exceptions.ShapeNullException;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.scene.input.MouseEvent;
@@ -23,6 +22,7 @@ public class MoveAction implements Action {
      * @param listInsertedShapes the list of already inserted shapes
      */
     public MoveAction(ObservableList<ShapeInterface> currentShape, ObservableList<ShapeInterface> listInsertedShapes) {
+        this.currentShape = currentShape;
         this.listInsertedShapes = listInsertedShapes;
         this.hasNotBeenExecuted = true;
     }
